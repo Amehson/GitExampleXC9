12b4eda9c7a5d981e72f9cf288d8383bdab27c99
Emmanuel Ameh <e.amehson@gmail.com>
2018-07-12 22:07:43 -0400

    Initial Commit

---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by Emmanuel Ameh on 7/12/18.
+//  Copyright © 2018 Emmanuel Ameh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
