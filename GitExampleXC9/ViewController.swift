f01c22768e3ae34265eb5b6e9fa81bb5ee328995
Emmanuel Ameh <e.amehson@gmail.com>
2018-07-12 22:15:11 -0400

    simple change in new branch

--- GitExampleXC9/ViewController.swift
+++ GitExampleXC9/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("Some Change")
+        print("Some Change in Emmanuel's Branch")
     }
 
     override var representedObject: Any? {
